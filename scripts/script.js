ba4ce81378a8d46601ff0a8a638b73782d79498f
mpapd <valkon.gr@gmail.com>
2020-10-26 01:17:37 +0200

    Closes #1

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,54 @@
 const CONTROVERSIAL_TITLE = "Controversial";
 const CONTROVERSIAL_URL = "/controversial/";
 const HOT_URL = "/hot/";
+const BEST_URL = "/best/";
+const TOP_URL = "/top/";
+const NEW_URL = "/new/";
+const REDDIT_BASE_URL =  "https://www.reddit.com";
 
+
+domObserver();
 
 var counter = 0;
+var currentItem = "";
+var controversialEnabled = false;
 
-findSortingParent(document.body)
+findHomePageSortingParent(document.body);
 
-function findSortingParent(element){
+
+function findHomePageSortingParent(element){
     if(element.hasChildNodes()){
         if(element.getAttribute("href") === HOT_URL){
             if(counter === 1){
-                //Checking if Controversial option is activated in order to deactivate it
-                //Reddit's controversial button exists two steps above.
-                var menuParentDiv = element.parentElement.parentElement.childNodes;
-                if(menuParentDiv[2].getAttribute("href") != CONTROVERSIAL_URL){
-                    addControversialItem(element);
+                /*  Handling when controversial options activated
+                    Checking if controversial option is activated in order to deactivate it.
+                    Two cases when controversial item present:
+                     - Reddit's controversial button exists two steps above the menu items.
+                     - Switching between items
+                */
+
+                
+                /**
+                 * Handling controversial when switching between items
+                 */
+                var menuParentDiv = element.parentElement.childNodes;
+                for(let menuItem of menuParentDiv){
+                    if(menuItem.getAttribute("href") === CONTROVERSIAL_URL){
+                        console.log("Mutation: EINAI MESA");
+                        controversialEnabled = true;
+
+                    }
                 }
+                if(!controversialEnabled){
+                    /**
+                     * Handling Reddit's controversial item, which appears two steps above.
+                     */
+                    var menuGrandParentDiv = element.parentElement.parentElement.childNodes;
+                    if(menuGrandParentDiv[2].getAttribute("href") !== CONTROVERSIAL_URL){
+                        addControversialItem(element);
+                    }
+                }
+
             }
             else{
                 counter++;
@@ -24,18 +56,17 @@ function findSortingParent(element){
             
         }
         else{
-            element.childNodes.forEach(findSortingParent);
+            element.childNodes.forEach(findHomePageSortingParent);
         }
-        
         
     }
     
 }
 
-/* 
-    Adds the controversial item on the menu
-    @param element  
-*/
+/**
+ * Adds the controversial item on the menu
+ * @param {*} element 
+ */
 function addControversialItem(element){    
     /* Hyperlink properties */
     var controversialItem = document.createElement("a");
@@ -59,5 +90,46 @@ function addControversialItem(element){
     divControversialClass.className = currentDivElement.getAttribute("class");
     controversialItem.appendChild(divControversialClass);
 
-    element.parentElement.appendChild(controversialItem)
+    element.parentElement.appendChild(controversialItem);
+}
+
+
+
+/**
+ * Observes DOM changes. Needed because Reddit is one page application and doesn't refresh.
+ */
+function domObserver(){
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    var observer = new MutationObserver(function(mutations, observer) {
+        for(let mutation of mutations){
+            if(mutation.target.baseURI === REDDIT_BASE_URL+HOT_URL || 
+            mutation.target.baseURI === REDDIT_BASE_URL+BEST_URL || 
+            mutation.target.baseURI === REDDIT_BASE_URL+NEW_URL ||
+            mutation.target.baseURI === REDDIT_BASE_URL+TOP_URL ){
+
+                /**
+                 * There are multiple mutations with the same baseURI and loading the item multiple times isn't efficient
+                **/
+                if(currentItem !== mutation.target.baseURI){
+                    currentItem = mutation.target.baseURI;
+                    console.log("Mutation: ", currentItem);
+                    counter = 0;
+                    findHomePageSortingParent(document.body);
+                    
+                }
+                
+            }
+        }
+        
+    });
+
+    observer.observe(document, {
+        subtree: true,
+        attributes: true,
+        attributeOldValue: true,
+        childList: true,
+        characterData: true, 
+        attributeFilter: ['data-thing']
+    });
 }
