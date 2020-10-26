a3b13abc40c3d776364f37ed65714d9859156a52
mpapd <valkon.gr@gmail.com>
2020-10-26 20:42:30 +0200

    First implementation

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const BEST_URL = "/best/";
 const TOP_URL = "/top/";
 const NEW_URL = "/new/";
 const REDDIT_BASE_URL =  "https://www.reddit.com";
+const SEPARATOR = "/";
 
 
 
@@ -118,14 +119,35 @@ function domObserver(){
                 }
                 
             }
-            if( mutation.target.baseURI === REDDIT_BASE_URL+"/"){
+            /**
+             * Handling home button
+             */
+            if(mutation.target.baseURI === REDDIT_BASE_URL+SEPARATOR){
                 if(currentItem !== null && currentItem !== mutation.target.baseURI ){ //currentItem won't be null when tab is active
                     counter = 0;
                     currentItem = mutation.target.baseURI;
                     findHomePageSortingParent(document.body);
+
                 }
                 
             }
+
+
+            
+            /**
+             * Handling subbreddits 
+             */
+            // if(mutation.target.baseURI === REDDIT_BASE_URL+"/r/reddevils/"){
+            //     if(true ){ //currentItem won't be null when tab is active
+            //         counter = 0;
+            //         currentItem = mutation.target.baseURI;
+            //         console.log("MUTAT SUBBB:",mutation.target.baseURI );
+            //         findHomePageSortingParent(document.body);
+            //     }
+
+            // }
+
+            
         }
 
         
